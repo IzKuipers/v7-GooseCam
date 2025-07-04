231b1e9e181534f2a640de1e1e3a9571c6532bd9
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-07-04 14:54:59 +0200

    Single instance

--- src/process.js
+++ src/process.js
@@ -2,22 +2,35 @@ const { GooseTray } = await load("tray.js");
 const { join } = util;
 
 class GooseCamProc extends Process {
-	constructor(handler, pid, parentPid) {
-		super(handler, pid, parentPid);
-	}
+  constructor(handler, pid, parentPid) {
+    super(handler, pid, parentPid);
+  }
 
-	async start() {
-		const shell = this.handler.getProcess(+env.get("shell_pid"));
-		const icon = await this.fs.direct(join(workingDirectory, "goosecam.png"));
+  async start() {
+    if (+this.env.get("goosecam_pid")) return false;
 
-		shell.trayHost.createTrayIcon(this.pid, "GooseTray", {
-			icon,
-			popup: {
-				width: 320,
-				height: 240
-			}
-		}, GooseTray);
-	}
+    this.env.set("goosecam_pid", this.pid);
+
+    const shell = this.handler.getProcess(+env.get("shell_pid"));
+    const icon = await this.fs.direct(join(workingDirectory, "goosecam.png"));
+
+    shell.trayHost.createTrayIcon(
+      this.pid,
+      "GooseTray",
+      {
+        icon,
+        popup: {
+          width: 320,
+          height: 240,
+        },
+      },
+      GooseTray
+    );
+  }
+
+  async stop() {
+    this.env.delete("goosecam_pid");
+  }
 }
 
 return { GooseCamProc };
